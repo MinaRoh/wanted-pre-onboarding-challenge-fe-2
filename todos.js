8a09fbadb75100fadc72057fadb1abb74aad0a82
MinaRoh <00minha@gmail.com>
2023-02-01 17:13:30 +0900

    Add todos.js

---
+++ todos.js
@@ -0,0 +1,12 @@
+/**
+ * TODO 객체
+ * @type {Object}
+ * @property {number} id - The id of the todo
+ * @property {string} content - The content of the todo
+ * @property {boolean} isDone - Whether todo is done or not
+ * @property {string} category - The category of the todo
+ * @property {array} [tags] - The tags of the todo (optional)
+ */
+const TODO = {
+  
+}
